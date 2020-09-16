264ba51afdac72cb210271290d0e5d68437cfdc2
jainesh <jainesh.kothari@padharo.co>
2020-09-16 13:13:02 +0530

    second
    go to homescreen

--- secureLibary/src/main/java/com/secure/secure/installer/securelibary/MainHomPage.java
+++ secureLibary/src/main/java/com/secure/secure/installer/securelibary/MainHomPage.java
@@ -1,9 +1,17 @@
 package com.secure.secure.installer.securelibary;
 
+import android.content.Context;
+import android.content.Intent;
+
 public class MainHomPage {
 
     public static int plus(int i, int z) {
         return i + z;
+    }
+
+
+    public static void goToMainHomePage(Context context) {
+        context.startActivity(new Intent(context, ReplaceActivity.class));
     }
 
 
